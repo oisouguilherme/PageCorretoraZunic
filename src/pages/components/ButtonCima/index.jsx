a896f30709303ea55295c9e204d93c0ca59f65dd
Guilherme Henrique <gh.dourado77@gmail.com>
2023-06-02 13:30:12 -0300

    alterando função para subir ao topo

--- src/pages/components/ButtonCima/index.jsx
+++ src/pages/components/ButtonCima/index.jsx
@@ -1,15 +1,14 @@
-import { Link } from "react-scroll";
-
 export default function ButtonCima(){
+  const handleScrollToTop = () => {
+     window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
   return (
-    <div className="fixed bottom-6 right-6 bg-slate-50 rounded-full p-1 cursor-pointer">
-      <Link
-      activeClass="active"
-      to="Nav"
-      spy={true}
-      smooth={true}
-      offset={50}
-      duration={500}>
+    <div className="fixed bottom-6 right-6 bg-slate-50 rounded-full flex p-1 cursor-pointer">
+      <button onClick={handleScrollToTop}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -24,7 +23,7 @@ export default function ButtonCima(){
             d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-      </Link>
+      </button>
     </div>
   );
 }
